66b69467240ef985afdf4e25756e9e70bf67c75f
bobdaduck <jonathanrhansen@yahoo.com>
2021-04-09 16:51:52 -0600

    remove comment

--- browser.py
+++ browser.py
@@ -11,8 +11,5 @@ class Driver:
         self.driver = webdriver.Chrome(options=browserOptions)
         self.driver.implicitly_wait(7)
 
-        # browser.get(BASE_URL)
-        # return browser
-
     def get_instance(self):
         return self.driver
