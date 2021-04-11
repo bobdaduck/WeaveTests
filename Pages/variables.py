c0acfb79bebac16c1bebd0c258a5b310ec92b349
bobdaduck <jonathanrhansen@yahoo.com>
2021-04-10 23:42:26 -0600

    add env variables for creating user and jwt token

--- Pages/variables.py
+++ Pages/variables.py
@@ -1,7 +1,10 @@
-#This file imports our environment variables to be used by each class
+# This file imports our environment variables to be used by each class
 import os
 
 URL = os.getenv('URL')
 
 EMAIL = os.getenv('EMAIL')
+EMAIL_FOR_GENERATOR = os.getenv('GENERATOR_EMAIL')  # format: youremail+{rnd}@gmail.com
 PASSWORD = os.getenv('PASSWORD')
+
+JWT_TOKEN = os.getenv("JWT")
