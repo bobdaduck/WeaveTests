3379f132486b3e28ef6f474cf778ebb3b0303f9d
bobdaduck <jonathanrhansen@yahoo.com>
2021-04-11 02:13:31 -0600

    add second simple response code test, expandable

--- Test_Cases/test_login_API.py
+++ Test_Cases/test_login_API.py
@@ -1,6 +1,5 @@
 import unittest
 import requests
-import json
 from Pages.variables import EMAIL
 from Pages.variables import PASSWORD
 from Pages.variables import JWT_TOKEN
@@ -38,19 +37,24 @@ class LoginTests(unittest.TestCase):
                 "content-type": "application/json",
                 }
 
-        data = {"username": "jonathanrhansen@yahoo.com",
-                "password": "getweave"}
+        data = {"username": EMAIL,
+                "password": PASSWORD}
 
         response = requests.post(self.login_url, headers=headers, json=data)
-        print(response.text)
         assert response.status_code == 200
-        # NOTE: Most API calls will require the `Location-Id` header to be set.
-        # r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-        # r.status_code
-        # r.headers['content-type']
-        # r.text
-        # r.json()
-        pass
+
+    def test_login_API_bad(self):
+        headers = {
+                "Authorization": "Bearer {}".format(JWT_TOKEN),
+                "Location-Id": "",
+                "content-type": "application/json",
+                }
+
+        data = {"username": "bad_email@yahoo.com",
+                "password": "bad_password"}
+
+        response = requests.post(self.login_url, headers=headers, json=data)
+        assert response.status_code == 401
 
     def tearDown(self):
         pass
